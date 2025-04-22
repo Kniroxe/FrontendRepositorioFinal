8801e9df67f09b9424498c50771c7e5ec461be4c
Kniroxe <leo.ooo2008@hotmail.com>
2025-04-22 09:10:46 -0600

    Primer commit angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ProductosComponent } from './components/productos/productos.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientesComponent,
+    ProductosComponent,
+    PedidosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
