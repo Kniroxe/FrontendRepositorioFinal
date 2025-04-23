b7e14f8de13796432a8de3f9905cfe85bee171b5
cyan-sp <cyan.mv@gmail.com>
2025-04-23 01:55:20 -0600

    feat(productos): implementar operaciones CRUD en vista de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { ProductosComponent } from './components/productos/productos.component';
 import { PedidosComponent } from './components/pedidos/pedidos.component';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientesComponent,
-    ProductosComponent,
     PedidosComponent,
     NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule,
+    HttpClientModule,
+    ProductosComponent,
   ],
   providers: [provideHttpClient()],
   bootstrap: [AppComponent]
